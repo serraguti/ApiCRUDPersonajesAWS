3d4d7a72101cf810ecc9b827c31006740b1a2fa8
serraguti <pacoserranox@gmail.com>
2022-05-10 13:17:05 +0200

    commit

---
+++ ApiCRUDPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,64 @@
+﻿using ApiCRUDPersonajesAWS.Models;
+using ApiCRUDPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCRUDPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private RepositoryPersonajes repo;
+
+        public PersonajesController(RepositoryPersonajes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Personaje>> Get()
+        {
+            return this.repo.GetPersonajes();
+        }
+
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public ActionResult<List<Personaje>> BuscarPersonajes(int id)
+        {
+            return this.repo.GetPersonajes();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Personaje> Find(int id)
+        {
+            return this.repo.FindPersonaje(id);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Personaje personaje)
+        {
+            this.repo.InsertPersonaje(personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Edit(Personaje personaje)
+        {
+            this.repo.ModificarPersonaje(personaje.IdPersonaje
+                , personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            this.repo.EliminarPersonaje(id);
+            return Ok();
+        }
+    }
+}
--- ApiCRUDPersonajesAWS/Startup.cs
+++ ApiCRUDPersonajesAWS/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiCRUDPersonajesAWS.Data;
+using ApiCRUDPersonajesAWS.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,14 @@ namespace ApiCRUDPersonajesAWS
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            string cadena = Configuration.GetConnectionString("AwsMySql");
+            services.AddTransient<RepositoryPersonajes>();
+            services.AddDbContext<PersonajesContext>
+                (options => options.UseMySql(cadena, ServerVersion.AutoDetect(cadena)));
+            services.AddCors(x =>
+            {
+                x.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
             services.AddControllers();
         }
 
@@ -36,7 +47,7 @@ namespace ApiCRUDPersonajesAWS
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(options => options.AllowAnyOrigin());
             app.UseHttpsRedirection();
 
             app.UseRouting();
